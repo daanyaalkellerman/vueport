7b8c956786ba9db210db7967fcbea90bcfc07818
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2023-12-18 11:59:37 +0200

    vueport

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>This is my home portfolio</h1>
+    <p>
+    i'll put something in here later when im home
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
